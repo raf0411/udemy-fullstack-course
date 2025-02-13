39c738cc928e10fad78cb2a9ae48a16c3eae05ee
Raffi <raffidarmawan0411@gmail.com>
2025-02-13 14:44:43 +0700

    completed cascade css

---
+++ dummy/main.js
@@ -0,0 +1,9 @@
+const intern = {
+  name: "John",
+  age: 30,
+  company: "PT. Kalbe Farma tbk",
+};
+
+function getInternDetails(intern) {
+  return `${intern.name} is ${intern.age} years old and works at ${intern.company}.`;
+}
