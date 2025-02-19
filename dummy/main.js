b3e33a460eb84b56f3bef3a7ba9180e751009780
Raffi <raffidarmawan0411@gmail.com>
2025-02-19 14:14:02 +0700

    completed css float

--- dummy/main.js
+++
@@ -1,9 +0,0 @@
-const intern = {
-  name: "John",
-  age: 30,
-  company: "PT. Kalbe Farma tbk",
-};
-
-function getInternDetails(intern) {
-  return `${intern.name} is ${intern.age} years old and works at ${intern.company}.`;
-}
